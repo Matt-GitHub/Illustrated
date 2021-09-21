6aed7054a0af240131fef62938a4abc31b41f7b0
Matt-GitHub <matthewjamesbedard@outlook.com>
2021-09-21 11:17:05 -0400

    updated hook

--- src/Hooks/FetchCourse.js
+++ src/Hooks/FetchCourse.js
@@ -6,11 +6,12 @@ function useCourse() {
   return useQuery(
     'sll data',
     () => {
+      console.log('process.env.REACT_APP_ACCESS_TOKEN', process.env.REACT_APP_ACCESS_TOKEN)
       return axios
         .get(
           `https://cdn.contentful.com/spaces/${process.env.REACT_APP_SPACE_ID}/environments/${process.env.REACT_APP_ENVIORNMENT}/entries?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`
         )
-        .then(res => res.data.items[1].fields);
+        .then(res => res.data.items[0].fields);
     },
     {
       staleTime: 1000 * 60 * 60
